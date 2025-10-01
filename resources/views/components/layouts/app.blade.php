8f6af2a14f56c176a1bfa2015393f726768d2832
Matheus Silva <ms25022003@gmail.com>
2025-10-01 11:22:23 -0300

    feat(layout): app background gradient overlay

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -56,6 +56,9 @@ declare(strict_types=1);
         <x-guess-container-layout>
             {{ $slot }}
         </x-guess-container-layout>
+        <div
+            class="from-indigo-primary to-app-gradient-end pointer-events-none fixed top-0 left-0 h-full w-full bg-gradient-to-br opacity-[4%]"
+        ></div>
         @filamentScripts
         @vite('resources/js/app.js')
     </body>
