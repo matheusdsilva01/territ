9ef48ddff1cb21dd737c587dbe235492b97a7a2c
Matheus Silva <ms25022003@gmail.com>
2025-10-06 09:23:23 -0300

    fix(card-stats): styling with variables color

--- resources/views/components/card-stats.blade.php
+++ resources/views/components/card-stats.blade.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 ?>
+
 @props([
     'color' => 'indigo-primary',
     'icon' => 'lucide-building-2',
@@ -11,10 +12,11 @@ declare(strict_types=1);
 ])
 
 <div
-    class="border-{{ $color }}/30 from-{{ $color }}/[8%] to-{{ $color }}/0 max-w-[320px] rounded-xl border bg-gradient-to-r p-8"
+    class="max-w-[320px] rounded-xl border border-[var(--color-variant)]/30 bg-gradient-to-r from-[var(--color-variant)]/[8%] to-[var(--color-variant)]/0 p-8"
+    style="--color-variant: {{ 'var(--color-' . $color }})"
 >
     <div class="flex gap-3">
-        <div class="bg-{{ $color }} rounded-sm p-2.5">
+        <div class="rounded-sm bg-[var(--color-bg)] p-2.5" style="--color-bg: {{ 'var(--color-' . $color }})">
             <x-dynamic-component :component="$icon" class="size-5 text-white" />
         </div>
         <div class="flex flex-col gap-1">
@@ -23,4 +25,5 @@ declare(strict_types=1);
         </div>
     </div>
 </div>
-<?php 
+
+<?php
