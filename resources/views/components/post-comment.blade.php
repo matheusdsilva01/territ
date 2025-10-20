2fc58a4c4febe6dcb602fdb0b1311be6ddeead1c
Matheus Silva <ms25022003@gmail.com>
2025-10-20 16:52:00 -0300

    feat(post): post content and comments

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -7,17 +7,17 @@ declare(strict_types=1);
 <div class="flex flex-row gap-3 rounded-xl [&+div[data-comment]]:ml-11">
     <div class="relative isolate shrink-0 basis-8">
         <img src="https://cdn-icons-png.flaticon.com/32/10851/10851235.png" alt="logo community" class="block" />
-        @if ($hasReplies)
+        @if ($comment->comments->count() > 0)
             <span
                 class="bg-helper-outline absolute top-8 right-1/2 left-1/2 h-[calc(100%+(var(--spacing)*8/2))] w-[1px] -translate-x-1/2"
             ></span>
         @endif
     </div>
-    <div class="flex flex-col gap-1">
+    <div class="flex w-full flex-col gap-1">
         <div class="flex items-center gap-3">
-            <p class="font-secondary">NexTurHe4rt</p>
-            <p class="text-3xs text-c-medium font-semibold">Há 3 dias</p>
-            @if ($isReply)
+            <p class="font-secondary">{{ $comment->author->username }}</p>
+            <p class="text-3xs text-c-medium font-semibold">{{ $comment->created_at->diffForHumans() }}</p>
+            @if (! is_null($comment->comment_parent_id))
                 <span
                     class="text-2xs rounded-xl border border-green-500/15 bg-green-500/10 px-3 py-1 font-semibold text-green-500"
                 >
@@ -32,16 +32,12 @@ declare(strict_types=1);
             @endif
             <x-lucide-ellipsis class="text-text-high ms-auto size-6" />
         </div>
-        <p class="leading-xs text-c-medium font-medium">
-            Recently, I had a task of learning a new tool that I had never used or seen anything related to, and I
-            thought: why not write about how I learned it? This is the kind of article that teaches you to learn things
-            from a different perspective. Not the best way, but you can reuse some of the concepts written here.
-        </p>
+        <p class="leading-xs text-c-medium font-medium">{{ $comment->content }}</p>
         <div class="flex gap-5">
-            <button class="text-2xs flex cursor-pointer items-center gap-2 p-2">
+            <span class="text-2xs flex items-center gap-2 p-2">
                 <x-lucide-message-circle class="size-4 text-white" />
-                1
-            </button>
+                {{ $comment->comments->count() }}
+            </span>
             <button class="text-2xs flex cursor-pointer items-center gap-2 p-2">
                 <x-lucide-thumbs-up class="size-4 text-white" />
             </button>
@@ -52,10 +48,10 @@ declare(strict_types=1);
         </div>
     </div>
 </div>
-@if ($replies)
+@if ($comment->comments->count() > 0)
     <div class="ml-11 flex flex-col gap-4">
-        @foreach ($replies as $reply)
-            <x-post-comment :replies="$reply['replies']" :is-reply="true" />
+        @foreach ($comment->comments as $reply)
+            <x-post-comment :comment="$reply" />
         @endforeach
     </div>
 @endif
