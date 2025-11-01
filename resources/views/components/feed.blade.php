04da166d90933508fc59881632d39c414947a42c
Matheus Silva <ms25022003@gmail.com>
2025-11-01 18:49:57 -0300

    refactor(feed): remove php component

--- resources/views/components/feed.blade.php
+++ resources/views/components/feed.blade.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 ?>
 
+@props([
+    'title' => 'Feed',
+    'posts' => [],
+])
 <x-content-wrapper :title="$title">
     @foreach ($posts as $post)
         <x-post-item :post="$post" />
