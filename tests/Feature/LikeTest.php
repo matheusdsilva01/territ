830d1a334d57359ad0f6701653fdeabc3459e083
Matheus Silva <ms25022003@gmail.com>
2025-11-03 11:03:49 -0300

    test(like): test for liking a comment

---
+++ tests/Feature/LikeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Community;
+use App\Models\Post;
+use App\Models\User;
+
+test('should like a comment', function (): void {
+    $community = Community::factory()->create();
+    $post = Post::factory()->for($community)->create();
+    $comment = $post->comments()->create([
+        'author_id' => User::factory()->create()->id,
+        'content' => 'This is a test comment.',
+    ])->first();
+    $anotherUser = User::factory()->create();
+
+    $response = $this->actingAs($anotherUser)->post(route('post.comment.like', [
+        'post' => $post->id,
+        'community' => $community->id,
+        'comment' => $comment->id,
+    ]));
+
+    $response->assertStatus(302);
+});
